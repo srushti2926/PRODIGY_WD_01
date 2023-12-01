13fd33e768ecc8863b2fd8db2cf23607f13644f7
srushti2926 <86220166+srushti2926@users.noreply.github.com>
2023-12-02 00:03:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById('booking-form').addEventListener('submit', function(event) {
+   event.preventDefault();
+   alert('Booking submitted!');
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+   event.preventDefault();
+   alert('Contact information submitted!');
+});
